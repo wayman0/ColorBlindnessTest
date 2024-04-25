49f6bd8faa99653deb6298133291753bae7aaab6
Chris Wayman <wayman0@pnw.edu>
2024-04-24 19:04:43 -0500

    started working on draggable boxes

---
+++ Slides/index.js
@@ -0,0 +1,91 @@
+const row0D = document.getElementById("row0");
+const row0B = row0D.getElementsByClassName("rowButton");
+
+const row1D = document.getElementById("row1");
+const row1B = row1D.getElementsByClassName("rowButton");
+
+const row2D = document.getElementById("row2");
+const row2B = row2D.getElementsByClassName("rowButton");
+
+const buttons = document.getElementsByClassName("rowButton");
+
+let dragged;
+let buttonOver;
+//randomizeRows();
+for(const button of buttons)
+{
+    button.addEventListener("dragstart", handleDragStart);
+    button.addEventListener("dragover",  handleDragOver);
+    button.addEventListener("dop",       handleDrop);
+}
+
+function randomizeRows()
+{
+    randomizeRow0();
+    randomizeRow1();
+    randomizeRow2();
+}
+
+function randomizeRow0()
+{
+    for(let swap = 0; swap < row0B.length; swap += 1)
+    {
+        const rIndex1 = Math.trunc(Math.random() * (row0B.length-1));
+        const rIndex2 = Math.trunc(Math.random() * (row0B.length-1));
+
+        const b1 = row0B[rIndex1];
+        const b2 = row0B[rIndex2];
+
+        const tempColor = getComputedStyle(b1)['backgroundColor'];
+        b1.style.backgroundColor = getComputedStyle(b2)['backgroundColor']
+        b2.style.backgroundColor = tempColor;
+    }
+}
+
+function randomizeRow1()
+{
+    for(let swap = 0; swap < row1B.length; swap += 1)
+    {
+        const rIndex1 = Math.trunc(Math.random() * (row1B.length-1));
+        const rIndex2 = Math.trunc(Math.random() * (row1B.length-1));
+
+        const b1 = row1B[rIndex1];
+        const b2 = row1B[rIndex2];
+        
+        const tempColor = getComputedStyle(b1)['backgroundColor'];
+        b1.style.backgroundColor = getComputedStyle(b2)['backgroundColor']
+        b2.style.backgroundColor = tempColor;
+    }
+}
+
+function randomizeRow2()
+{
+    for(let swap = 0; swap < row2B.length; swap += 1)
+    {
+        const rIndex1 = Math.trunc(Math.random() * (row2B.length-1));
+        const rIndex2 = Math.trunc(Math.random() * (row2B.length-1));
+
+        const b1 = row2B[rIndex1];
+        const b2 = row2B[rIndex2];
+
+        const tempColor = getComputedStyle(b1)['backgroundColor'];
+        b1.style.backgroundColor = getComputedStyle(b2)['backgroundColor']
+        b2.style.backgroundColor = tempColor;
+    }
+}
+
+function handleDragStart(e)
+{
+    dragged = e.target;
+}
+
+function handleDragOver(e)
+{
+    e.preventDefault();
+    
+}
+
+function handleDrop(e)
+{
+    e.preventDefault();
+}
