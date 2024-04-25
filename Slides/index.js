4aa056df2c491fcd3e7e962c5932474c81e138da
Chris Wayman <wayman0@pnw.edu>
2024-04-24 20:08:49 -0500

    added check button

--- Slides/index.js
+++ Slides/index.js
@@ -1,18 +1,59 @@
 const row0D = document.getElementById("row0");
 const row0B = row0D.getElementsByClassName("rowButton");
+const r0Check = document.getElementById("r0Check");
 
 const row1D = document.getElementById("row1");
 const row1B = row1D.getElementsByClassName("rowButton");
+const r1Check = document.getElementById("r1Check");
 
 const row2D = document.getElementById("row2");
 const row2B = row2D.getElementsByClassName("rowButton");
+const row2Check = document.getElementById("r2Check");
 
 const buttons = document.getElementsByClassName("rowButton");
+
+const rgRowColors = new Array(2);
+rgRowColors[0] = new Array(row0B.length);
+rgRowColors[1] = new Array(row0B.length);
+
+const gbRowColors = new Array(2);
+gbRowColors[0] = new Array(row1B.length);
+gbRowColors[1] = new Array(row1B.length);
+
+const brRowColors = new Array(2);
+brRowColors[0] = new Array(row2B.length);
+brRowColors[1] = new Array(row2B.length);
+
+for(let index = 0; index < rgRowColors[0].length; index += 1)
+{
+    rgRowColors[0][index] = 255 - (10 * index);
+    rgRowColors[1][index] =   0 + (10 * index);
+
+    gbRowColors[0][index] = 255 - (10 * index);
+    gbRowColors[1][index] =   0 + (10 * index);
+
+    brRowColors[0][index] = 255 - (10 * index);
+    brRowColors[1][index] =   0 + (10 * index);
+}
+
+rgRowColors[0][rgRowColors[0].length-1] = 255;
+rgRowColors[1][rgRowColors[1].length-1] = 255;
+
+gbRowColors[0][rgRowColors[0].length-1] = 255;
+gbRowColors[1][rgRowColors[1].length-1] = 255;
+
+brRowColors[0][rgRowColors[0].length-1] = 255;
+brRowColors[1][rgRowColors[1].length-1] = 255;
 
 let dragged;
 let buttonOver;
 
-randomizeRows();
+//randomizeRows();
+
+r0Check.addEventListener("click", checkRow0);
+r1Check.addEventListener("click", checkRow1);
+r2Check.addEventListener("click", checkRow2);
+
 
 for(const button of buttons)
 {
@@ -95,4 +136,76 @@ function handleDragOver(e)
 function handleDrop(e)
 {
     e.preventDefault();
+}
+
+function checkRow0(e)
+{
+    let correct = true;
+    for(let index = 0; index < row0B.length; index += 1)
+    {
+        const style = getComputedStyle(row0B[index])['backgroundColor'];
+        
+        const startP = style.indexOf('(');
+        const endP = style.indexOf(')');
+        
+        const rgbStr = style.substring(startP+1, endP);
+        const rgbArr = rgbStr.split(", ");
+        
+        const rStr = rgbArr[0];
+        const gStr = rgbArr[1];
+
+        if(! (rStr == rgRowColors[0][index] &&
+              gStr == rgRowColors[1][index]))
+                correct = false;
+    }
+
+    alert(correct? "You Got This Row Right":"You Got This Row Wrong");
+}
+
+function checkRow1(e)
+{
+    let correct = true;
+    for(let index = 0; index < row1B.length; index += 1)
+    {
+        const style = getComputedStyle(row1B[index])['backgroundColor'];
+        
+        const startP = style.indexOf('(');
+        const endP = style.indexOf(')');
+        
+        const rgbStr = style.substring(startP+1, endP);
+        const rgbArr = rgbStr.split(", ");
+        
+        const gStr = rgbArr[1];
+        const bStr = rgbArr[2];
+
+        if(! (gStr == gbRowColors[0][index] &&
+              bStr == gbRowColors[1][index]))
+                correct = false;
+    }
+
+    alert(correct? "You Got This Row Right":"You Got This Row Wrong");
+}
+
+function checkRow2(e)
+{
+    let correct = true;
+    for(let index = 0; index < row2B.length; index += 1)
+    {
+        const style = getComputedStyle(row2B[index])['backgroundColor'];
+        
+        const startP = style.indexOf('(');
+        const endP = style.indexOf(')');
+        
+        const rgbStr = style.substring(startP+1, endP);
+        const rgbArr = rgbStr.split(", ");
+        
+        const bStr = rgbArr[2];
+        const rStr = rgbArr[0];
+
+        if(! (bStr == brRowColors[0][index] &&
+              rStr == brRowColors[1][index]))
+                correct = false;
+    }
+
+    alert(correct? "You Got This Row Right":"You Got This Row Wrong");
 }
