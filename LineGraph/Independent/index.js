3e302cc8b91fa7a82df05810e384c55083b7a5ea
Chris Wayman <wayman0@pnw.edu>
2024-11-25 21:18:48 -0600

    made one show/hide button instead of one for each

--- LineGraph/Independent/index.js
+++ LineGraph/Independent/index.js
@@ -3,7 +3,6 @@ const row0D = document.getElementById("row0Butt");
 const row0B = row0D.getElementsByClassName("rowButton");
 const r0Check = document.getElementById("r0Check");
 const r0Show = document.getElementById("r0Show");
-const r0Hide = document.getElementById("r0Hide");
 const r0Results = document.getElementById("r0Results");
 
 const row1S = document.getElementById("row1Show");
@@ -11,7 +10,6 @@ const row1D = document.getElementById("row1Butt");
 const row1B = row1D.getElementsByClassName("rowButton");
 const r1Check = document.getElementById("r1Check");
 const r1Show = document.getElementById("r1Show");
-const r1Hide = document.getElementById("r1Hide");
 const r1Results = document.getElementById("r1Results");
 
 const row2S = document.getElementById("row2Show")
@@ -19,7 +17,6 @@ const row2D = document.getElementById("row2Butt");
 const row2B = row2D.getElementsByClassName("rowButton");
 const r2Check = document.getElementById("r2Check");
 const r2Show = document.getElementById("r2Show");
-const r2Hide = document.getElementById("r2Hide");
 const r2Results = document.getElementById("r2Results");
 
 const buttons = document.getElementsByClassName("rowButton");
@@ -57,13 +54,9 @@ r0Check.addEventListener("click", checkRow0);
 r1Check.addEventListener("click", checkRow1);
 r2Check.addEventListener("click", checkRow2);
 
-r0Show.addEventListener("click", () => {row0S.style.visibility = "visible";});
-r1Show.addEventListener("click", () => {row1S.style.visibility = "visible";});
-r2Show.addEventListener("click", () => {row2S.style.visibility = "visible";});
-
-r0Hide.addEventListener("click", () => {row0S.style.visibility = "hidden";});
-r1Hide.addEventListener("click", () => {row1S.style.visibility = "hidden";});
-r2Hide.addEventListener("click", () => {row2S.style.visibility = "hidden";});
+r0Show.addEventListener("click", () => {row0S.style.visibility == "visible" ? row0S.style.visibility = "hidden" : row0S.style.visibility = "visible";});
+r1Show.addEventListener("click", () => {row1S.style.visibility == "visible" ? row1S.style.visibility = "hidden" : row1S.style.visibility = "visible";});
+r2Show.addEventListener("click", () => {row2S.style.visibility == "visible" ? row2S.style.visibility = "hidden" : row2S.style.visibility = "visible";});
 
 r0Results.addEventListener("click", seeResults);
 r1Results.addEventListener("click", seeResults);
